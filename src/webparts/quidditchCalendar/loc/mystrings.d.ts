bdce757600b7489a4439991c38ea1a60e12b2192
Christopher Øyen <christopher.oyen@inmeta.no>
2025-01-23 13:48:52 +0100

    Initial commit

---
+++ src/webparts/quidditchCalendar/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IQuidditchCalendarWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'QuidditchCalendarWebPartStrings' {
+  const strings: IQuidditchCalendarWebPartStrings;
+  export = strings;
+}
